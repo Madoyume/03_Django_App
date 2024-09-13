efdf1e760fa2d9d416e77154e03ae07a734e258c
Mado <Kotsukisan@gmail.com>
2024-09-13 22:46:05 +0900

    2024.09.13 mado Viewクラスを利用したHTTPレスポンス実装

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
+from django.http import HtttpResponse
 from .forms import HelloForm
+from django.http import TemplateView
 
-def index(request):
-    params = {
-        'title':'Hello/Index',
-        'message':'your data',
-        'form':HelloForm(),
-    }
-    if (request.method == 'POST'):
-        params['message'] = '名前:' + request.POST['name'] + \
-            '<br>メール:' + request.POST['mail'] + \
-            '<br>年齢:' + request.POST['age']
-        params['form'] = HelloForm(request.POST)
-    return render(request, 'hello/index.html', params)
+class HelloView(TemplateView):
+    
+    def __init__(self):
+        self.params = {
+            'title':'Hello',
+            'message':'your data',
+            'form':HelloForm()
+        }
+    
+    def get(self, request):
+        return render(request, 'hello/index.html', self.params)
+
+    def post(self, request):
+        msg = f'あなたは、<b>{request.POST['name']}({request.POST['age']})さんです。' + \
+            f'<br>メールアドレスは<b>{request.POST['mail']}ですね。'
+        self.params['message'] = msg
+        self.params['form'] = HelloForm(request.POST)
+        return render(request, 'hello/index.html', self.params)
