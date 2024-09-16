c8628cbea5471b4d798b916f608a889c44d54c12
Mado <Kotsukisan@gmail.com>
2024-09-16 11:17:36 +0900

    2024.09.16 mado モデル内データを画面表示

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
-from .forms import SessionForm
-from django.views.generic import TemplateView
+from .models import Friend
 
-class HelloView(TemplateView):
-    
-    def __init__(self):
-        self.params = {
-            'title':'Hello',
-            'form':SessionForm(),
-            'result':None
-        }
-    
-    def get(self, request):
-        self.params = request.session.get('last_msg', 'No message.')
-        return render(request, 'hello/index.html', self.params)
-
-    def post(self, request):
-        ses = request.POST['session']
-        self.params['result'] = f'send:"{ses}".'
-        request.session['last_msg'] = ses
-        self.params['form'] = SessionForm(request.POST)
-        return render(request, 'hello/index.html', self.params)
+def index(request):
+    data = Friend.objects.all()
+    params = {
+        'title':'Hello',
+        'message':'all friends.',
+        'data':data,
+    }
+    return render(request, 'hello/index.html', params)
