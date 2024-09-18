c2c6d8114d11382ca068e48edc04b7dbbf7c032e
Mado <Kotsukisan@gmail.com>
2024-09-18 23:28:57 +0900

    2024.09.18 mado 画面上でデータ検索

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -1,8 +1,4 @@
 from django import forms
 
 class HelloForm(forms.Form):
-    check = forms.BooleanField(label='CheckBox', required=False)
-
-class SessionForm(forms.Form):
-    session = forms.CharField(label='session', required=False, \
-        widget=forms.TextInput(attrs={'class':'form-control'}))
+    id = forms.IntegerField(label='ID')
