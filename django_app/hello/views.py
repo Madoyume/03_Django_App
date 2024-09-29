00c54d3ee8238c42211c5ff8e0abbb48d456cbf8
Mado <Kotsukisan@gmail.com>
2024-09-29 15:45:35 +0900

    2024.09.29 mado 要素計算の実装

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -5,6 +5,7 @@ from .forms import FriendForm
 from django.views.generic import ListView
 from django.views.generic import DetailView
 from .forms import FindForm
+from django.db.models import Count,Sum,Avg,Min,Max
 
 class FriendList(ListView):
     model = Friend
@@ -13,9 +14,20 @@ class FriendDetail(DetailView):
     model = Friend
 
 def index(request):
-    data = Friend.objects.all().order_by('age')
+    data = Friend.objects.all()
+    re1 = Friend.objects.aggregate(Count('age'))
+    re2 = Friend.objects.aggregate(Sum('age'))
+    re3 = Friend.objects.aggregate(Avg('age'))
+    re4 = Friend.objects.aggregate(Min('age'))
+    re5 = Friend.objects.aggregate(Max('age'))
+    msg = 'count:' + str(re1['age__count']) \
+        + '<br>Sum:' + str(re2['age__sum']) \
+        + '<br>Average:' + str(re3['age__avg']) \
+        + '<br>Min:' + str(re4['age__min']) \
+        + '<br>Max:' + str(re5['age__max']) 
     params = {
         'title':'Hello',
+        'message':msg,
         'data':data,
     }
     return render(request, 'hello/index.html', params)
