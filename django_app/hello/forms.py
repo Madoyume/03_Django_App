3e7e7076adb8a88c087c81b6f29c1890ad982a5d
Mado <Kotsukisan@gmail.com>
2024-10-04 00:03:22 +0900

    2024.10.03 mado 独自バリデーションの実装

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -11,13 +11,11 @@ class FindForm(forms.Form):
         widget=forms.TextInput(attrs={'class':'form-control'}))
 
 class CheckForm(forms.Form):
-    requeired = forms.IntegerField(label='Requeired', \
-        widget=forms.NumberInput(attrs={'class':'form-control'}))
-    min = forms.IntegerField(label='Min', min_value=100, \
-        widget=forms.NumberInput(attrs={'class':'form-control'}))
-    max = forms.IntegerField(label='Max', max_value=1000, \
-        widget=forms.NumberInput(attrs={'class':'form-control'}))
-    date = forms.DateField(label="Date", input_formats=['%d'], \
-        widget=forms.DateInput(attrs={'class':'form-control'}))
-    time = forms.TimeField(label='Time', \
-        widget=forms.TimeInput(attrs={'class':'form-control'}))
+    str = forms.CharField(label='String', \
+        widget=forms.TextInput(attrs={'class':'form-control'}))
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        str = cleaned_data['str']
+        if (str.lower().startswith('no')):
+            raise forms.ValidationError('You input "No"!')
