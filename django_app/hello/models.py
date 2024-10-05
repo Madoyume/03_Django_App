3b0fb2ea84dfba95e7bcdc4d823380c405332cb0
Mado <Kotsukisan@gmail.com>
2024-10-05 20:23:30 +0900

    2024.10.05 mado 正規表現の活用

--- django_app/hello/models.py
+++ django_app/hello/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator
+from django.core.validators import (
+    MinValueValidator, 
+    MaxValueValidator, 
+    MinLengthValidator,
+    RegexValidator
+)
 
 class Friend(models.Model):
     name = models.CharField(max_length=100, \
-        validators=[MinLengthValidator(10)])
+        validators=[MinLengthValidator(10), \
+            RegexValidator(r'^[a-z]*$')])
     mail = models.EmailField(max_length=200, \
         validators=[MinLengthValidator(10)])
     gender = models.BooleanField()
